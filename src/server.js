0068c5bbe08675f7f7aad762296134295abba082
Aras <aras.opraza@gmail.com>
2024-02-19 09:54:11 +0700

    Starter project

--- src/server.js
+++ src/server.js
@@ -1,9 +1,6 @@
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
 const NotesService = require('./services/NotesService');
-const logger = require('./logger/index');
-const ClientError = require('./exceptions/ClientError');
-const os = require('os');
 require('dotenv').config();
 
 const init = async () => {
@@ -23,22 +20,6 @@ const init = async () => {
     },
   )
   
-  server.ext('onPreResponse', (request, h) => {
-    const { response } = request;
-
-    if (response instanceof ClientError) {
-      const newResponse = h.response({
-        status: 'fail',
-        message: response.message,
-      });
-      newResponse.code(response.statusCode);
-      return newResponse;
-    }
-
-    logger.info(`userIP=${request.info.remoteAddress}, host=${os.hostname},  method=${request.method}, path=${request.path}, payload=${JSON.stringify(response.output.payload)}`);
-    return h.continue;
-  });
-
   await server.start();
   console.log(`server start at ${server.info.uri}`);
 }
